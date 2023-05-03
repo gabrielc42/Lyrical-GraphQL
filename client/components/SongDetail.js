542982fa072a198eebb25334c0467d541918d513
gabrielc42 <clark.gabe97@gmail.com>
2023-05-03 12:01:28 -0500

    navigating between screens, lyric creation form, createlyric mutation, submitting mutations, showing list of lyrics, enhancing queries, middle of identifying records

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -9,10 +12,12 @@ class SongDetail extends Component {
     if (!song) { return <div> Loading... </div> }
 
     return (
-
       <div>
+        <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-      </div>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id} />
+      </div >
     );
   }
 }
